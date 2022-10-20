565478740551072c1afd7d886ee715a9faddd94f
EugeneAlWin <evgenijgoncarevic@gmail.com>
2022-10-20 12:10:50 +0300

    Labs 6-7 done

---
+++ Lab7/Lab.sql
@@ -0,0 +1,30 @@
+--1
+--list of the background tasks
+select * from v$bgprocess;
+
+--2
+--tasks that are working now
+select * from v$bgprocess where paddr != '00';
+
+--3
+--count of DBWn processes working now
+select count(*) from v$bgprocess where paddr!= '00' and name like 'DBW%';
+
+--4, 5
+--connections with instance and its modes
+select username, status, server from v$session where username is not null;
+
+--6
+select * from v$services;
+
+--7
+show parameter dispatcher;
+
+--8
+--show listener
+select * from v$services;
+-- orcl
+
+--9
+--connections with instance
+select * from v$session where username is not null;
